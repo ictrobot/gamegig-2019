5ad26a670353b70d84298db74b04281ae6f2f223
Elanor <els72@cam.ac.uk>
2019-12-06 16:50:55 +0000

    score added

---
+++ code/score.lua
@@ -0,0 +1,29 @@
+local class = require 'code/lib/middleclass'
+
+
+Score = class('Score')
+
+function Score:initialize(const)
+    self.score = 0
+    self.target = 100
+    self.const = const
+end
+
+function Score:draw()
+    love.graphics.setColor(0.1, 0.8, 0.1)
+    love.graphics.rectangle("fill", 20, 100, 50, 200)
+    love.graphics.setColor(1,1,1)
+    local fraction = math.max(self.score/self.target, 0.01)
+    love.graphics.rectangle("fill", 20, 100, 50, 200 - 200*fraction)
+end
+
+function Score:add(score_to_add)
+    self.score = math.min(self.target,self.score + score_to_add)
+end
+
+function Score:subtract(score_to_subtract)
+    self.score = math.max(0,self.score - score_to_subtract)
+end
+
+
+return Score
