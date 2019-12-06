0368db16b8b970191d0ca901ab058a3412ec1b08
ictrobot <ictrobot@outlook.com>
2019-12-06 12:25:41 +0000

    Update World

--- code/world.lua
+++ code/world.lua
@@ -3,7 +3,7 @@ local class = require 'code/lib/world.lua'
 World = class('World')
 
 function World:initialize()
-    self.distanceGenerated = 0
+    self.distanceGenerated = -1
     self.tiles = {}
     self.height = 12
 end
@@ -12,8 +12,14 @@ function World:setTile(x, y, tile)
     self.tiles[x * self.height + y] = tile
 end
 
-function World:getTile(x, y, tile)
+function World:getTile(x, y)
     return self.tiles[x * self.height + y]
+end
+
+function World:generateColumn()
+    self.distanceGenerated = self.distanceGenerated + 1
+    
+    self:setTile(self.distanceGenerated, 0, {})
 end
 
 return World
